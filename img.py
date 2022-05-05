6ac576709f56f7e119fcacd7933a2ab74f99acd2
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2022-05-05 09:32:08 +0530

    Update img.py

--- img.py
+++ img.py
@@ -7,7 +7,7 @@ while True:
      cv2.imshow('FRAME',frame)
      if cv2.waitKey(1) == ord('a'):
         print ("pressed a")
-        frame=cv2.imwrite("/home/pi/facedistance-mediapipe/rf.png",frame)
+        frame=cv2.imwrite("/home/pi/hand-distance/rf.png",frame)
         break
         
 cap.release()
