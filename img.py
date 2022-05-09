e7043dcb2f40125e7e7d6409e88e5d1ee742979c
leroy.zwakman <leroy.zwakman@topicus.nl>
2022-05-09 16:10:54 +0200

    moved to /tmp

--- img.py
+++ img.py
@@ -7,8 +7,8 @@ while True:
      cv2.imshow('FRAME',frame)
      if cv2.waitKey(1) == ord('a'):
         print ("pressed a")
-        frame=cv2.imwrite("/home/pi/hand-distance/rf.png",frame)
+        frame=cv2.imwrite("/tmp/rf.png",frame)
         break
-        
+
 cap.release()
 cv2.destroyAllWindows()
